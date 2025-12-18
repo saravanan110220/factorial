24df78122677d30f2ad946bf2093debb12397a31
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-18 08:42:18 +0530

    initial commit: version2

--- factorial.c
+++ factorial.c
@@ -9,13 +9,6 @@ unsigned long long factorial_iterative(int n) {
     return result;
 }
 
-unsigned long long factorial_recursive(int n) {
-    if (n == 0 || n == 1)
-        return 1;
-    else
-        return n * factorial_recursive(n - 1);
-}
-
 int main() {
     int choice, number;
     unsigned long long result;
@@ -23,8 +16,7 @@ int main() {
     while (1) {
         printf("\n========== FACTORIAL PROGRAM ==========\n");
         printf("1. Calculate factorial (Iterative Method)\n");
-        printf("2. Calculate factorial (Recursive Method)\n");
-        printf("3. Exit\n");
+        printf("2. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -36,22 +28,11 @@ int main() {
                     printf("Error: Factorial of negative numbers doesn't exist!\n");
                 } else {
                     result = factorial_iterative(number);
-                    printf("Factorial of %d (Iterative) = %llu\n", number, result);
+                    printf("Factorial of %d = %llu\n", number, result);
                 }
                 break;
 
             case 2:
-                printf("Enter a non-negative integer: ");
-                scanf("%d", &number);
-                if (number < 0) {
-                    printf("Error: Factorial of negative numbers doesn't exist!\n");
-                } else {
-                    result = factorial_recursive(number);
-                    printf("Factorial of %d (Recursive) = %llu\n", number, result);
-                }
-                break;
-
-            case 3:
                 printf("Exiting program... Goodbye!\n");
                 exit(0);
 
